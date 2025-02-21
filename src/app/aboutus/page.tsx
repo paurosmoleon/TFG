c27129ce70219c9eafe5626d70e5f5cf6b095758
fergone03 <114108805+fergone03@users.noreply.github.com>
2025-02-21 18:53:26 +0100

    Implementar estructura básica de la aplicación con navegación a Dashboard y Sobre Nosotros

---
+++ src/app/aboutus/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutUs() {
+    return (
+      <main>
+        <h1>Esto es una prueba para ver como funciona react con next porque no lo he tocado en la vida</h1>
+      </main>
+    );
+  }
+  
