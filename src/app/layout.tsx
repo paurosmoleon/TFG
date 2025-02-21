9c8e279b11b7829b60b183ce09ee702208ea06d9
fergone03 <114108805+fergone03@users.noreply.github.com>
2025-02-21 19:18:46 +0100

    Agregar componente Footer con navegación y logo de Docker

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function RootLayout({
   children,
@@ -11,6 +12,7 @@ export default function RootLayout({
       <body className="antialiased">
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
