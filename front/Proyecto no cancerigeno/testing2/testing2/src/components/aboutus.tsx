78e1b128b123951a091b8299cca95c8a695b0f5a
fergone03 <fergonesteban03@gmail.com>
2025-04-21 14:38:34 +0200

    testing quitados

--- front/Proyecto no cancerigeno/testing2/testing2/src/components/aboutus.tsx
+++
@@ -1,8 +0,0 @@
-export default function AboutUs() {
-    return (
-      <main>
-        <h1>Esto es una prueba para ver como funciona react con next porque no lo he tocado en la vida</h1>
-      </main>
-    );
-  }
-  
--- front/Proyecto no cancerigeno/testing2/testing2/src/components/dashbboard.tsx
+++
@@ -1,9 +0,0 @@
-export default function Dashboard() {
-    return (
-      <main>
-        <h1>Dashboard</h1>
-        <p>Mi chamba</p>
-      </main>
-    );
-  }
-  
