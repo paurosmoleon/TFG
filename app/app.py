15c2c3b81df1fffae9d195bfdfdcbd222a886b49
Perandres8 <77155860+Perandres8@users.noreply.github.com>
2025-05-09 12:21:31 +0200

    t

--- app/app.py
+++ app/app.py
@@ -9,15 +9,11 @@ app.include_router(user_routes.router,prefix='/users',tags=['Users'])
 app.include_router(calendar_routes.router,prefix='/calendar',tags=['Calendar'])
 
 
-origins = [
-    "http://localhost:3000",  # por ejemplo si usas React localmente
-    "http://127.0.0.1:3000",
-    "https://tu-frontend-produccion.com"
-]
+
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  # puedes usar ["*"] para permitir todos (NO recomendado en producción)
+    allow_origins=['*'],  # puedes usar ["*"] para permitir todos (NO recomendado en producción)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
