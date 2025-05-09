a42ec842b14023203f4be14feb74015b618c3442
Perandres8 <77155860+Perandres8@users.noreply.github.com>
2025-05-09 12:28:30 +0200

    test cords

--- app/app.py
+++ app/app.py
@@ -13,7 +13,7 @@ app.include_router(calendar_routes.router,prefix='/calendar',tags=['Calendar'])
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['88.30.81.205'],  # puedes usar ["*"] para permitir todos (NO recomendado en producción)
+    allow_origins=['88.30.81.205:0'],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
