55dc7d246cc2de964996ecf17edc4d61f5049295
fergone03 <114108805+fergone03@users.noreply.github.com>
2025-05-22 19:28:02 +0200

    Refactor: Implementar componente PageNotFound con diseño y navegación mejorados

--- front/src/components/PageNotFound.tsx
+++ front/src/components/PageNotFound.tsx
@@ -1,7 +1,40 @@
-const PageNotFound = () => {
-  return (
-    <div>PageNotFound</div>
-  )
-}
+import { Link, useNavigate } from 'react-router-dom';
 
-export default PageNotFound
+const PageNotFound = () => {
+  const navigate = useNavigate();
+
+  return (
+    <section className="bg-white">
+      <div className="container flex items-center min-h-screen px-6 py-12 mx-auto">
+        <div>
+          <p className="text-sm font-medium text-blue-500">Error 404</p>
+          <h1 className="mt-3 text-2xl font-semibold text-gray-800 md:text-3xl">
+            No podemos encontrar la página que buscas
+          </h1>
+          <p className="mt-4 text-gray-500">
+            Lo sentimos, la página que estás buscando no existe o ha sido
+            movida.
+          </p>
+
+          <div className="flex items-center mt-6 gap-x-3">
+            <button
+              className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto hover:bg-gray-100 cursor-pointer"
+              onClick={() => navigate(-1)}
+            >
+              <span>Volver</span>
+            </button>
+
+            <Link
+              to="/home"
+              className="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto hover:bg-blue-600 cursor-pointer flex items-center justify-center"
+            >
+              Ir a inicio
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default PageNotFound;
