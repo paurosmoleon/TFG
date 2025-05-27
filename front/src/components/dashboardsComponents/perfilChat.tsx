c88c1f26c9b1da882dbf10053b3faa36cdc59749
fergone03 <114108805+fergone03@users.noreply.github.com>
2025-05-27 13:19:37 +0200

    Refactor: Actualizar la lógica para obtener el usuario actual en el componente PerfilChat

--- front/src/components/dashboardsComponents/perfilChat.tsx
+++ front/src/components/dashboardsComponents/perfilChat.tsx
@@ -1,13 +1,38 @@
+import { useParams } from 'react-router-dom';
 import Profile from './Profile';
+import { useEffect, useState } from 'react';
+
 
 const PerfilChat: React.FC = () => {
+  const [currentUser, setCurrentUser] = useState('')
+
+  useEffect(() => {
+    const CurrentUser = async () => {
+      const user = await axios.get('https://tfg-production-f839.up.railway.app/users/me', {
+        headers: {
+          Authorization: localStorage.getItem('tokenUser')
+        }
+      })
+
+      setCurrentUser(user.data)
+    }
+
+
+  }, [])
+
+  console.log('Token en la URL:', '');
+
+  // que hace / dice
+
+  //prueba ahora lo he comentado yo , el nombre se muestra? tardo un poco que me han deslogeado
+  // mira dc
   return (
     <Profile
-      name="Juan Pérez"
-      phone="+34 600 123 456"
-      dni="12345678A"
-      role="teacher_class"
-      empresa="Empresa XYZ"
+      name={currentUser.name}
+      phone="user.phone"
+      dni="user.dni"
+      role="student"
+    //empresa={user.empresa || null}
     />
   );
 };
