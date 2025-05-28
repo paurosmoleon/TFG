0d90708cece8b148405c013cb61ce5d8ecf0f27f
Alejandro <acr0029@alu.medac.es>
2025-05-28 09:00:56 +0200

    pruieba

--- front/src/components/Tiptap.tsx
+++ front/src/components/Tiptap.tsx
@@ -1,24 +1,83 @@
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
-import { useCallback } from 'react'
+import { useCallback, useEffect, useState } from 'react'
+import axios from 'axios'
 
 const TiptapEditor = ({ onSave }: { onSave: (content: string) => void }) => {
+  const [currentUser, setCurrentUser] = useState()
+  const [teacherS, setTeacher] = useState()
+
   const editor = useEditor({
     extensions: [StarterKit],
     content: '<p>Escribe aquí...</p>',
   })
 
-  const handleSave = useCallback(() => {
-    if (editor) {
-      const html = editor.getHTML() // También puedes usar editor.getJSON() o editor.getText()
-      onSave(html) // Lo pasas al componente padre o haces algo con él
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const student = await axios.get('https://tfg-production-f839.up.railway.app/users/me', {
+          headers: {
+            Authorization: localStorage.getItem('tokenUser')
+          }
+        })
+        const current = student.data[0]
+        setCurrentUser(current)
+
+        const teacher = await axios.get(`https://tfg-production-f839.up.railway.app/AC/find_by_student/${currentUser['id']}`, {
+          headers: {
+            Authorization: localStorage.getItem('tokenUser')
+          }
+        })
+        const current2 = teacher.data['Message'][0]['id']
+        setTeacher(current2)
+
+      } catch (err) {
+        console.log(err)
+      }
     }
-  }, [editor, onSave])
+
+    fetch()
+  }, [])
+
+
+
+  const handleSave = async () => {
+    const obj = {
+      "student_id": currentUser['id'],
+      "school_tutor_id": teacherS,
+      "content": editor?.getHTML().toString()
+    }
+    console.log(obj)
+    // if (!currentUser || !teacherS) {
+    //   console.error('Datos no cargados aún')
+    //   return
+    // }
+
+    // try {
+
+
+    //   await axios.post('https://tfg-production-f839.up.railway.app/PD/create', obj, {
+    //     headers: {
+    //       Authorization: localStorage.getItem('tokenUser')
+    //     }
+    //   })
+
+    // } catch (err) {
+    //   console.log(err)
+    // }
+  }
+
+  //useCallback(() => {
+  //   if (editor) {
+  //     const html = editor.getHTML() // También puedes usar editor.getJSON() o editor.getText()
+  //     onSave(html) // Lo pasas al componente padre o haces algo con él
+  //   }
+  // }, [editor, onSave])
 
   return (
     <div>
       <EditorContent editor={editor} />
-      <button onClick={handleSave} className="mt-4 px-4 py-2 bg-blue-600 text-white rounded">
+      <button onClick={() => handleSave()} className="mt-4 px-4 py-2 bg-blue-600 text-white rounded">
         Guardar contenido
       </button>
     </div>
--- front/src/components/dashboardsComponents/memoriaPracticas.tsx
+++ front/src/components/dashboardsComponents/memoriaPracticas.tsx
@@ -1,12 +1,42 @@
+import { useEffect } from 'react';
 import TiptapEditor from '../Tiptap';
+import axios from 'axios';
 const memoriaPracticas = () => {
   const handleSaveContent = (content: string) => {
     console.log('Contenido guardado:', content);
   };
+
+
+  function downloadPDF() {
+    axios.get('https://tfg-production-f839.up.railway.app/PD/pdf/2', {
+      responseType: 'blob',  // ¡Importante!
+      headers: {
+        Authorization: localStorage.getItem('tokenUser')
+      }
+    })
+      .then((response) => {
+        const blob = new Blob([response.data], { type: 'application/pdf' });
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'reporte.pdf';
+        document.body.appendChild(a);
+        a.click();
+        a.remove();
+        window.URL.revokeObjectURL(url);
+      })
+      .catch((error) => {
+        console.error("Error al descargar el PDF", error);
+      });
+  }
+
+
+
   return (
     <div>
       <h1>Memoria Practicas</h1>
       <TiptapEditor onSave={handleSaveContent} />
+      <button onClick={downloadPDF} className='cursor-pointer'>asdasd </button>
     </div>
   );
 };
