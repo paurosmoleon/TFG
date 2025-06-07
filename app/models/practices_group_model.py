e90b604b74b3cdc235364065cf143f8226752e9e
Pau <pauros2004@gmail.com>
2025-06-07 20:46:27 +0200

    aasdasdasdasdasdqasdassfdsujnm

--- app/models/practices_group_model.py
+++ app/models/practices_group_model.py
@@ -2,6 +2,6 @@ from pydantic import BaseModel
 from datetime import date
 
 class practicesGroupModel(BaseModel):
-    practice_center_name: str
+    practices_center_name: str
     student_id: int
     company_tutor_id: int
