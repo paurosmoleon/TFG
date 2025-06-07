c5f1f86fb8752b42959be422621e8a9dac542fb8
fergone03 <114108805+fergone03@users.noreply.github.com>
2025-06-07 19:26:56 +0200

    Añadir jsPDF como dependencia y corregir la importación en FichaSemanal.

--- front/src/components/dashboardsComponents/fichaSemanal.tsx
+++ front/src/components/dashboardsComponents/fichaSemanal.tsx
@@ -2,7 +2,7 @@
 import { useState, useRef, ChangeEvent, FC } from "react";
 import SignatureCanvas from "react-signature-canvas";
 import { DiaData, FichaSemanalData, UserRole } from "../../types/ficha";
-import jsPDF from "jspdf";
+import jsPDF from "jsPDF";
 
 const diasSemana: Array<keyof FichaSemanalData["dias"]> = [
   "LUNES",
